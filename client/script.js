042f76c0499d50ac79e9bdddb5da03b224625e1e
prakashbt <53742462+prakashbt@users.noreply.github.com>
2023-01-25 23:53:54 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   // fetch the data from server : bot 
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codexai-vmgo.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
